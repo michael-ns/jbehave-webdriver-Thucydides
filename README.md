e8ee80764f3a1b7c1d47e3b2c45d05f3d2db7db9
michael-ns <michael.hou@inf.bulletproof.net>
2016-02-03 16:15:57 +1100

    working base, BOOM!

---
+++ README.md
@@ -0,0 +1,3 @@
+I am using IntelliJ
+
+Maven
---
+++ src/test/java/bpf/michaelhou/mythucydides/jbehave/AcceptanceTestSuite.java
@@ -0,0 +1,5 @@
+package bpf.michaelhou.mythucydides.jbehave;
+
+import net.thucydides.jbehave.ThucydidesJUnitStories;
+
+public class AcceptanceTestSuite extends ThucydidesJUnitStories {}
